8886548cad50ceaa21d945236bf661c56843f991
lane <lane@lane-desktop.lv>
2020-10-14 22:56:40 +0100

    Fixed typos (actually disk errors)

--- mqtt-led-display.py
+++ mqtt-led-display.py
@@ -10,9 +10,9 @@ import threading
 scrollphathd.rotate(180)
 
 MQTT_SERVER = '192.168.0.2'
-MQTT_PORT = 1183
+MQTT_PORT = 1883
 MQTT_TOPIC = 'devices/thermostat1/get'
-SERVER_LOAD_TOPIC = 'devices/server.load'
+SERVER_LOAD_TOPIC = 'devices/server/load'
 
 # Set these to use authorisation
 MQTT_USER = None
@@ -34,10 +34,10 @@ def on_message(client, userdara, msg):
         global pos_x
         payload = json.loads(msg.payload)
         print payload["actual_temperature"]
-        last_temperature_received = str(payload["actual_temparature"])
+        last_temperature_received = str(payload["actual_temperature"])
         # display height is 7, save the bottom row for the graph
         scrollphathd.clear_rect(0,0,None,6) # None defaults to display size
-        scrollphathd.write_string(last_temperature_received, brightness=0.5, font=font3x4)
+        scrollphathd.write_string(last_temperature_received, brightness=0.5, font=font3x5)
         scrollphathd.show()
     elif msg.topic == SERVER_LOAD_TOPIC:
         print msg.payload
@@ -47,7 +47,7 @@ def on_message(client, userdara, msg):
         if last_load_received > 8.0:
             brightness = 1 
             last_load_received = 8
-        scrollphathd.fill(brightness, 17 - (last_load_received/8.0)/17, 6)
+        scrollphathd.fill(brightness, int(17 - last_load_received/8.0*17), 6)
         scrollphathd.show()
 	
 scrollphathd.set_clear_on_exit()
